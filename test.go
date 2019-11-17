e1d4c7ece5ca59b8b7e4c0e4964b812cabfc5c24
Kanokpol.Cs <57785933+autzxzxxop1123@users.noreply.github.com>
2019-11-17 19:11:39 +0700

    test

---
+++ test.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var age int = 21
+	var money float64 = 940.23
+	//วิธีลัด
+	age1 := 21
+	money0 := 940.23
+	fmt.Println(age)
+	fmt.Println(money)
+	fmt.Println(age1)
+	fmt.Println(money0)
+}
+	
+
