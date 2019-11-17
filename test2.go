c7af468aac22bd6c11f6e495286eb0173b274c60
Kanokpol.Cs <57785933+autzxzxxop1123@users.noreply.github.com>
2019-11-17 19:12:16 +0700

    test2

---
+++ test2.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	//ชุดข้อความ
+	Z1 := "Kanokpol" 
+	Z2 := "chomboon" 
+	//นำข้อความมาต่อกัน
+	Z3 := Z1 + Z2
+	fmt.Println(Z3)
+	//การปริ้นตัวที่กำหนด
+	fmt.Println(Z1[1:4])
+	fmt.Println(Z3[0:5])
+	fmt.Println(Z3[1:6])
+}
+	
+
