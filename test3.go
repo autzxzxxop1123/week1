35c3dd37d3b862e54624e42ce57caf48f8df79f9
Kanokpol.Cs <57785933+autzxzxxop1123@users.noreply.github.com>
2019-11-17 19:12:32 +0700

    test3

---
+++ test3.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("\n \t kanokpol")	
+	fmt.Println(`\n \t kanokpol`)
+	fmt.Println()
+	fmt.Println(`\n \t kanokpol`[0])
+	fmt.Println(len(`\n \t kanokpol`))
+	fmt.Println("K"+"A")
+
+	
+}
+	
+
