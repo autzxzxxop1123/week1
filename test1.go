8a02b0c19597dc5d43b1a6974d98fa94f21ea719
Kanokpol.Cs <57785933+autzxzxxop1123@users.noreply.github.com>
2019-11-17 19:11:57 +0700

    test1

---
+++ test1.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	money1 := 20
+	money2 := 30
+	fmt.Println(money1 + money2)
+	fmt.Println(money2 - money1)
+	fmt.Println(money2 * money1)
+	fmt.Println(money2 / money1)
+}
+	
+
